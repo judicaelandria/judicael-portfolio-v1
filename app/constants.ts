a35aa41f4506ef524911f245c68ddc868e38734d
Judicael <46365844+judicaelandria@users.noreply.github.com>
2022-01-18 13:53:08 +0300

    fix description, social image (#4)
    Co-authored-by: judicael_ai <judicael@smartpredict.ai>

--- app/constants.ts
+++ app/constants.ts
@@ -3,4 +3,6 @@ export const GITHUB_URL = "https://github.com/judicaelandria";
 export const EMAIL = "judicaelandria7@gmail.com";
 export const DOMAINS = "https://judicael.vercel.app";
 
-export const CONTENT = "Judicaël digital garden";
+export const TITLE = "Judicaël A. -- Designer, developer";
+export const DESCRIPTION =
+  "Welcome to my digital garden, this is the place where I share some of  side projects. I love building and experimenting things to learn more about the web and mobile. I think this is the best way to learn for me. Dreamin up and building a project that I wish existed is my  passion or project that exist but I can do something to make it more awesome.";
