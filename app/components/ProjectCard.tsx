c6834bfffaf7295ab9aeaefb8ce7ad3eb653b224
Judicael <46365844+judicaelandria@users.noreply.github.com>
2022-02-15 10:30:45 +0300

    update link for the last project

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -17,7 +17,8 @@ const ProjectCard = ({ name, desc, logo, link }: IProjectCardProps) => {
           <img src={logo} alt={name} className="w-11 h-11 fill-white" />
           <div className="flex flex-col ml-4">
             <h4 className="font-poppins text-lg text-white">{name}</h4>
-            <p className="font-poppins text-white text-xs">{desc}</p>
+            <p className="font-poppins text-white text-xs">{desc} </p>
+            <span className="text-sx font-poppins text-white/80">ONGOING</span>
           </div>
         </div>
       }
